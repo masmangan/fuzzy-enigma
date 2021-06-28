516f0b887d93da63afb3cd68c8768c991e3ce988
Marco Mangan <marco.mangan@gmail.com>
2021-06-28 10:44:09 -0300

    Exceções e depositar e retirar valor.

--- BankAccount.java
+++ BankAccount.java
@@ -1,36 +1,112 @@
 // Direitos Autorais, 2021, Escola Politécnica, PUCRS
 
 /**
+ * A classe BankAccount...
+ * 
  * @author Marco Mangan (marco.mangan@pucrs.br)
+ * 
+ * Por determinação de Legislação, é obrigatório
+ * um CPF válido para cada conta de pessoa física.
  */
 public class BankAccount
 {
-    public double balance; // saldo da conta
-    public String cpf;
+    private double balance; // saldo da conta
+    private String cpf; // cpf do cliente
+      
+    /**
+     * Confere o cpf indicado.
+     * 
+     * @param cpf o cpf a ser conferido.
+     * 
+     * @exception IllegalArgumentException
+     */
+    private static void checkCpf(String cpf)
+    {
+        if (cpf == null)
+        {
+            throw new IllegalArgumentException("O CPF não pode ser null!");
+        }
+        if (cpf.length() != 14)
+        {
+            throw new IllegalArgumentException("O CPF deve ser composto de 11 caracteres!");            
+        }        
+    }
     
     /**
+     * Cadastra uma conta no cpf indicado, com um saldo de R$ 0,00.
      * 
+     * @exception IllegalArgumentException
      */
-    public BankAccount(double initialBalance, String novoCpf) // construtor
+    public BankAccount(double balance, String cpf) // construtor
     {
-        balance = initialBalance;
-        cpf = novoCpf;
+        checkCpf(cpf);
+        
+        this.balance = balance;
+        this.cpf = cpf;
     }
 
     /**
-     * 
+     * Cadastra uma conta no cpf indicado, 
+     * indicado com um saldo inicial de R$ 10.000,00.
+     * @exception IllegalArgumentException
      */
-    public BankAccount(String novoCpf) // construtor
+    public BankAccount(String cpf) // construtor
     {
-        balance = 10000.0;
-        cpf = novoCpf;
+        checkCpf(cpf);
+        
+        this.balance = 10000.0;
+        this.cpf = cpf;
     }    
     
     /**
+     * Consulta o saldo da conta.
      * 
+     * @return o valor do saldo da conta em reais.
      */
     public double getBalance()
     {
         return balance;
+    }
+    
+    /**
+     * Consulta o CPF do cliente desta conta.
+     * 
+     * @return o CPF do cliente
+     */
+    public String getCpf()
+    {
+        return cpf;
+    }
+    
+    /**
+     * Deposita um value ao saldo da conta.
+     * 
+     * @param value o valor a ser depositado, maior do que R$ 0,00
+     * @exception IllegalArgumentException
+     */
+    public void deposit(double value)
+    {
+        if (value <= 0)
+        {
+            throw new IllegalArgumentException();            
+        }
+        balance = balance + value;
+        // deposit() ver livro do Horstmann Capítulo 3   
+    }
+    
+    /**
+     * Retira um value do saldo da conta.
+     * 
+     * @param value o valor a ser depositado, maior do que R$ 0,00
+     * @exception IllegalArgumentException
+     */
+    public void withdraw(double value)
+    {
+        if (value <= 0)
+        {
+            throw new IllegalArgumentException();            
+        }
+        balance = balance - value;
+     // TODO: withdraw() ver livro do Horstmann Capítulo 3   
     }
 }
--- MainApp.java
+++ MainApp.java
@@ -13,11 +13,11 @@ public class MainApp
         BankAccount c1;  //
         BankAccount c2; //
         
-        c1 = new BankAccount("123456789-00");
-        c2 = new BankAccount(1600.00, "123456789-00");
+        c1 = new BankAccount("123.456.789-00");
+        c2 = new BankAccount(1600.00, "123.456.789-00");
         
         System.out.println(c1.getBalance());
-        System.out.println(c1.getBalance());
+        System.out.println(c2.getBalance());
         
         // BankAccount[] cs;
         // cs = new BankAccount[] { 
@@ -29,5 +29,8 @@ public class MainApp
         // {
             // System.out.println(x.getBalance());
         // }        
+        
+        // TODO: completar o programa para depositar 200 reais na primeira conta,
+        // 450 na segunda conta e mostrar o saldo de cada conta.
     }
 }
